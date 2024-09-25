94fc4c4c49a90d94a942c96778a43ab9a1d5b82d
AirSurfer09 <saurav@convai.com>
2024-09-25 19:54:20 +0530

    iframe messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+  const iframeUrl = "https://iframe-web-six.vercel.app/";
+
+  const sendMessageToIframe = () => {
+    if (iframeRef.current) {
+      iframeRef.current.contentWindow?.postMessage("Hello from parent!", iframeUrl); // Ensure to specify the iframe origin for security
+    }
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+        <iframe
+          ref={iframeRef}
+          style={{ height: window.innerHeight, width: window.innerWidth }}
+          id="Mantis"
+          frameBorder="0"
+          allowFullScreen
+          src={iframeUrl}
+        ></iframe>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={sendMessageToIframe}>
+          Send message from parent
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
