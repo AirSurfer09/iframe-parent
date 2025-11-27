b144b39537f1e92ec225793e3679a7ec9fd842da
Saurav <138126051+AirSurfer09@users.noreply.github.com>
2025-11-27 11:15:42 +0530

    Update ClientHome.tsx

--- src/app/ClientHome.tsx
+++ src/app/ClientHome.tsx
@@ -121,7 +121,7 @@ export default App;`,
 ];
 
 export default function ClientHome() {
-  const initialExpId = "01ad2713-e35b-4e79-9814-05db90bf1151";
+  const initialExpId = "2b0b1fac-cb54-11f0-84d6-42010a7be027";
   const pixelStreamRef = useRef<PixelStreamComponentHandles>(null);
   const copyToClipboard = (text: string) => {
     navigator.clipboard.writeText(text);
